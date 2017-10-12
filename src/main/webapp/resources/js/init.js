b4a352e586379ea3ebe3979188bb9f9ea6401dc7
Michael Ranaldo <michael@ranaldo.co.uk>
2017-10-12 11:40:08 +0100

    Added in my own guff - JSF for the base of the site and some moderately clunky QoL pages, plus cheap materialize theme

---
+++ src/main/webapp/resources/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    
+  }); // end of document ready
+  $(document).ready(function() {
+    $('select').material_select();
+});
+$(document).ready(function(){
+    $('.tooltipped').tooltip({delay: 50});
+});
+})(jQuery); // end of jQuery name space
