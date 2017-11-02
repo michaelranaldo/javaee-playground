a085b879d38fe2d6fd833b38bc903d82f29f9887
Michael Ranaldo <michael@ranaldo.co.uk>
2017-11-02 10:49:24 +0000

    Added JSON-P Examples

--- src/main/java/uk/co/ranaldo/javaeeplayground/jsonp/fileuploadBackingBean.java
+++ src/main/java/uk/co/ranaldo/javaeeplayground/jsonp/fileuploadBackingBean.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uk.co.ranaldo.javaeeplayground.jsonp;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.math.BigDecimal;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -18,20 +16,26 @@ import javax.json.JsonReader;
 import javax.servlet.http.Part;
 
 /**
- * 
+ * Backing bean for the json-p demo. Delightfully simple to use - just create a field or method here and set the
+ * action / method on the page.
  * @author Michael Ranaldo <michael@ranaldo.co.uk>
  */
 @Named("fileUploadBackingBean")
 @RequestScoped
 public class fileuploadBackingBean {
+
     private Part file;
     private String text;
-    private String parsedText;
-    
+    private String formattedText;
+    private String rawText;
+    private String aKey;
+    private String aValue;
+
     /**
      * Upload the file from the form, stick it here
+     *
      */
-    public void upload() {
+    public void formatFile() {
         if (file != null) {
             try {
                 InputStream inputStream = file.getInputStream();
@@ -41,18 +45,34 @@ public class fileuploadBackingBean {
             }
         }
     }
-    
+
+    public void formatText() {
+        if (rawText != null) {
+            JsonReader jsonReader = Json.createReader(new StringReader(rawText));
+            JsonObject jsonObject = jsonReader.readObject();
+            formattedText = jsonObject.toString();
+        }
+    }
+
     /**
      * Using the magic, read in our json object and spit it out as clean test
      */
     public void convertTextFromJSON() {
         try {
+            // Use JsonReader to read the stream
             JsonReader jsonReader = Json.createReader(file.getInputStream());
-            JsonObject jobj = jsonReader.readObject();
-            parsedText = jobj.toString();
+            // create an object from the stream
+            JsonObject jsonObject = jsonReader.readObject();
+            // print the object
+            formattedText = jsonObject.toString();
         } catch (IOException ex) {
             Logger.getLogger(fileuploadBackingBean.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public void createJson() {
+        JsonObject jsonObject = Json.createObjectBuilder().add(aKey, aValue).build();
+        formattedText = jsonObject.toString();
     }
 
     public Part getFile() {
@@ -71,12 +91,36 @@ public class fileuploadBackingBean {
         this.text = text;
     }
 
-    public String getParsedText() {
-        return parsedText;
+    public String getFormattedText() {
+        return formattedText;
     }
 
-    public void setParsedText(String parsedText) {
-        this.parsedText = parsedText;
+    public void setFormattedText(String formattedText) {
+        this.formattedText = formattedText;
     }
+
+    public String getRawText() {
+        return rawText;
+    }
+
+    public void setRawText(String rawText) {
+        this.rawText = rawText;
+    }
+
+    public String getaKey() {
+        return aKey;
+    }
+
+    public void setaKey(String aKey) {
+        this.aKey = aKey;
+    }
+
+    public String getaValue() {
+        return aValue;
+    }
+
+    public void setaValue(String aValue) {
+        this.aValue = aValue;
+    }
+
 }
-
